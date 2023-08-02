a473d897943dde79534455cf410f19a89b4fdf21
Bikash Shrestha <bik36245@gmail.com>
2023-08-02 17:41:04 +0545

    interaction

--- index.js
+++ index.js
@@ -17,38 +17,38 @@
 // // console.log(u);
 
 
-// // interaction
-//  const username = prompt("enter your username");
-//  const password = prompt("enter your password");
-//  const sure = confirm("are you sure");
-//  if (username==="Bikash" && password==="abc"){
-//  alert("Welcome to Heaven");}
+interaction
+  const username = prompt("enter your username");
+  const password = prompt("enter your password");
+   const sure = confirm("are you sure");
+  if (username==="Bikash" && password==="abc"){
+  alert("Welcome to Heaven");}
 
 
 
-// //operators
-// || (or)
-// && (and)
-// ! (not)
+ operators
+ || (or)
+ && (and)
+ ! (not)
 
-// const hour= prompt("enter hour");
-// if(hour>1 && hour<24)
-// {
-//     alert("Js is on the way");
-// }
-// else if(hour==1||hour==24)
-// {
-//     alert("Js class is closed");
-// }
-// else{
-//     alert("nothing");
-// }
+const hour= prompt("enter hour");
+if(hour>1 && hour<24)
+ {
+     alert("Js is on the way");
+ }
+else if(hour==1||hour==24)
+ {
+    alert("Js class is closed");
+ }
+  else{
+    alert("nothing");
+ }
 
 
-// const isvalid = true;
-// if (!isvalid)
-// {
-//     alert("happy");
-// }
+const isvalid = true;
+if (!isvalid)
+{
+     alert("happy");
+ }
 let s=5;
 s>0? alert("s is greater"):alert("s is not greater");
