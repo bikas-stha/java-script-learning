a8daa8c79d0030f04d2d6371aab9e71db8471812
Bikash Shrestha <bik36245@gmail.com>
2023-08-02 17:15:59 +0545

    interaction and operators

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-console.log("hello");
-var ram = "person";
-ram = "syam";
-console.log(ram);
+// // console.log("hello");
+// // var ram = "person";
+// // ram = "syam";
+// // console.log(ram);
 
 
 
-//variables(var, let, const)
-let a=5;
-let b=6;
-a=7;
-b=3;
-console.log(a);
-console.log(b);
+// // //variables(var, let, const)
+// // let a=5;
+// // let b=6;
+// // a=7;
+// // b=3;
+// // console.log(a);
+// // console.log(b);
 
-const u=6;
-console.log(u);
+// // const u=6;
+// // console.log(u);
+
+
+// // interaction
+//  const username = prompt("enter your username");
+//  const password = prompt("enter your password");
+//  const sure = confirm("are you sure");
+//  if (username==="Bikash" && password==="abc"){
+//  alert("Welcome to Heaven");}
+
+
+
+// //operators
+// || (or)
+// && (and)
+// ! (not)
+
+const hour= prompt("enter hour");
+if(hour>1 && hour<24)
+{
+    alert("Js is on the way");
+}
+else if(hour==1||hour==24)
+{
+    alert("Js class is closed");
+}
+else{
+    alert("nothing");
+}
+cont 
